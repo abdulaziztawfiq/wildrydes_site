77eba01edda43b81dcc0c8f8f318621052fddf18
abdulaziztawfiq <abdulaziztawfiqhn@gmail.com>
2025-02-09 09:49:10 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_0SWftsUaz', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4lgcdgdja4epq76e1m7a0a89kq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
